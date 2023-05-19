63db5819506d696251b538ada0ca51b42957cec8
JM Gustafson <jodymgustafson@hotmail.com>
2023-05-19 19:57:46 -0400

    added images

--- packages/code-city-beer/src/components/OrderList.tsx
+++ packages/code-city-beer/src/components/OrderList.tsx
@@ -13,12 +13,15 @@ export default function OrderList(props: OrderListProps) {
 
   return (
     <div className="orders-list">
-      {props.orderItems.length === 0 && <><span>There's nothing to see here!</span><span>&nbsp;</span></>}
+      {props.orderItems.length === 0 && <>
+        <span className="descr">I suggest you order a beer</span>
+        <span className="total">$0</span>
+      </>}
       {props.orderItems.map(i =>
-      <>
-        <span className="descr">{i.itemName}</span>
-        <span className="total">{i.count} @ ${i.costPerUnit} = ${i.count * i.costPerUnit}</span>
-      </>
+        <>
+          <span className="descr">{i.itemName}</span>
+          <span className="total">{i.count} @ ${i.costPerUnit} = ${i.count * i.costPerUnit}</span>
+        </>
       )}
       <span>&nbsp;</span><span className="total due">Total Due: ${totalDue}</span>
     </div>
--- packages/web-server/src/beers-repo.ts
+++ packages/web-server/src/beers-repo.ts
@@ -20,15 +20,6 @@ const beerData: Beer[] = [
         price: 7
     },
     {
-        id: "3",
-        name: "Pac-Man Pale Ale",
-        description: "A delightfully pleasant yellow hued ale--I ain't afraid of no ghosts.",
-        abv: "5.8%",
-        pintsRemaining: getRandomAmount(),
-        imageUrl: "pac-man.webp",
-        price: 6
-    },
-    {
         id: "4",
         name: "Galaga Lager",
         description: "Clear and crisp with a foamy head, and the taste of alien butt being blown to bits.",
@@ -38,15 +29,6 @@ const beerData: Beer[] = [
         price: 5
     },
     {
-        id: "5",
-        name: "Space Invaders IPA",
-        description: "A hopped up ale with hints of pine and grapefruit--just the thing for taking on an alien invasion.",
-        abv: "7.2%",
-        pintsRemaining: getRandomAmount(),
-        imageUrl: "invaders.webp",
-        price: 6
-    },
-    {
         id: "6",
         name: "Ms. Pac-Man Fruit Ale",
         description: "A fruity ale with berries, cherries and a hint of ghost pepper. A sophisticated yet down-to-earth brew.",
@@ -54,6 +36,24 @@ const beerData: Beer[] = [
         pintsRemaining: getRandomAmount(),
         imageUrl: "ms-pac-man.webp",
         price: 7
+    },
+    {
+        id: "3",
+        name: "Pac-Man Pale Ale",
+        description: "A delightfully pleasant yellow hued ale--I ain't afraid of no ghosts.",
+        abv: "5.8%",
+        pintsRemaining: getRandomAmount(),
+        imageUrl: "pac-man.webp",
+        price: 6
+    },
+    {
+        id: "5",
+        name: "Space Invaders IPA",
+        description: "A hopped up ale with hints of pine and grapefruit--just the thing for taking on an alien invasion.",
+        abv: "7.2%",
+        pintsRemaining: getRandomAmount(),
+        imageUrl: "invaders.webp",
+        price: 6
     },
 ];
 
