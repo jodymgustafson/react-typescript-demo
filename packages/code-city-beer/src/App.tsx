6e53f75eb021b632a11f12e39647c5c9e44f2441
JM Gustafson <jodymgustafson@hotmail.com>
2023-07-03 15:33:25 -0400

    cleanup

--- packages/code-city-beer/src/App.tsx
+++ packages/code-city-beer/src/App.tsx
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
-import './App.css';
 import { Outlet } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Outlet/>
-      {/* {view === "home" && <WelcomeView onStartOrder={startOrder}/>}
-      {view === "menu" && <MenuView table={table} orderName={orderName} onViewTab={() => setView("tab")}/>}
-      {view === "tab" && <OrderView table={table} orderName={orderName} onClose={() => setView("menu")} onTabClosed={() => setView("home")}/>}
-      {view === "beer" && <BeerDetails table={table} orderName={orderName} beerId='' />} */}
     </div>
   );
 }
