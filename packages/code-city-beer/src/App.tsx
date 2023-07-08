ec3cc1467d1b36cb29f7379a8d90c70a6c673113
JM Gustafson <jodymgustafson@hotmail.com>
2023-07-08 12:46:08 -0400

    rename

--- packages/code-city-beer/src/App.tsx
+++ packages/code-city-beer/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Welcome from './components/Welcome';
+import Welcome from './components/WelcomeView';
 
 function App() {
   return (
